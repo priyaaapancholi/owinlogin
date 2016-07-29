1e47e81e5df0b4cded50a02fb034f770e13822f7
Priya <priya.pancholi@promactinfo.com>
2016-07-29 11:22:52 +0530

    login

--- MyFollowOwin/Controllers/AccountController.cs
+++ MyFollowOwin/Controllers/AccountController.cs
@@ -231,7 +231,7 @@ namespace MyFollowOwin.Controllers
             if (ModelState.IsValid)
             {
                 MailMessage m = new MailMessage(
-                new MailAddress("priya.pancholi@promactinfo.com", "Priya Mail"),
+                new MailAddress("karan.desai@promactinfo.com", "Priya Mail"),
                 new MailAddress(user.Email));
                 m.Subject = "Confirmation Mail";
                 m.Body = string.Format("Dear {0}<BR/>Thank you for your registration,Click to Confirm Email <a href=\"{1}\" title=\"User Email Confirm\">{1}</a>", user.UserName, Url.Action("ConfirmEmail", "Account", new { Token = user.Id, Email = user.Email }, Request.Url.Scheme));
